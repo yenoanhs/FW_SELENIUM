6fdef4a0e9abc0305efc36ddda7bd9b3ae2b6d14
yenoanhs <oanhnny@gmail.com>
2018-02-28 17:35:48 +0700

    unit test 2 tuncs of ExcelUtils

---
+++ src/main/java/testUnit/testUnitExcelFunc.java
@@ -0,0 +1,27 @@
+package testUnit;
+
+import customLibs.ExcelUtils;
+
+public class testUnitExcelFunc {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		String filepath="E:/oanh/FW_SELENIUM/src/testUnit/testData.xlsx";
+		String sheetname="Sheet1";
+		ExcelUtils oExcel = new ExcelUtils(filepath, sheetname);
+		int numberRow =oExcel.getRowCount();
+		System.out.println("Num of row =" + numberRow);
+		for (int i=2; i<=numberRow; i++)
+		{
+			//can use func getColFromName(String) to find what colNumber of col username, password
+			// col start with 0, row start with 1 (header)
+			String user = oExcel.getCellData(i, 0);
+			String pass = oExcel.getCellData(i, 1);
+			System.out.println("user = "+user+", pass = "+pass);
+		}
+
+	}
+
+
+}
